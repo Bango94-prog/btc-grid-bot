5c8d3ad22bcf55420c5f6488ca968b717ba933d7
Bango94-prog <antonellorizzo87@gmail.com>
2025-05-10 11:57:54 +0200

    Delete dashboard_minimal_test.py

--- dashboard_minimal_test.py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="TEST BASE", layout="centered")
-st.title("✅ Dashboard attiva")
-st.write("Se vedi questo messaggio, Streamlit funziona correttamente.")
